854ebb1dd31e50a03a7e53f6f4b512d8e82bf54d
Iskandar <anatraxia@Iskandars-MacBook-Pro.local>
2017-01-15 03:31:46 +0800

    update JScript

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,45 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
+if (cardTwo === cardFour) {
+	window.alert("You found a match!");
+}
+else
+{
+	window.alert("Sorry, try again.");
+}
+if (cardTwo === cardOne ){
+	window.alert("You found a match!");
+}
+else
+{
+	window.alert("Sorry, try again.");
+}
+if (cardTwo === cardThree ){
+	window.alert("You found a match!");
+}
+else
+{
+	window.alert("Sorry, try again.");
+}
+if (cardOne === cardFour) {
+	window.alert("You found a match!");
+}
+else
+{
+	window.alert("Sorry, try again.");
+}
+if (cardOne === cardThree ){
+	window.alert("You found a match!");
+}
+else
+{
+	window.alert("Sorry, try again.");
+}
+if (cardFour === cardThree ){
+	window.alert("You found a match!");
+}
+else
+{
+	window.alert("Sorry, try again.");
+}
